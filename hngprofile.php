392da6873b3f161c298c92060c599c901fc0e662
idiftech2010 <idiftech2010@yahoo.com>
2017-08-22 16:35:53 +0100

    my profile page

---
+++ hngprofile.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Idris Ibriaheem Personal Profle</title>
+    <link href="style.css" type="stylesheet" />
+</head>
+<body>
+    <h2>Idris Ibraheem Profile</h2>
+    <img src="profile.jpg" alt="Idress" title="Idris" />
+    <hr />
+    
+    <p>
+        Name: <br />
+        Slack: 
+        Github: <a href="https://github.com/idfitech2010"></a>
+        
+        
+    
+    </p>
+
+
+</body>
+
+</html>
